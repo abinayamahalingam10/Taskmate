60339cb10b40b28e89d789b8ee017fef0c93eec0
abinayamahalingam10 <abinaya.mahalingam10@gmail.com>
2022-02-02 23:56:58 +0530

    modified models.py

--- todolist_app/models.py
+++ todolist_app/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class TaskList(models.Model):
-    manage = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+    #manage = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
     task = models.CharField(max_length=300)
     done = models.BooleanField(default=False)
     
